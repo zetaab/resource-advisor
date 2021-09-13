5a3d133d7d11524bec5a013cc8260ef23567185f
Jesse Haka <haka.jesse@gmail.com>
2021-09-13 08:17:24 +0300

    daemonsets and statefulsets

--- pkg/advisor/main.go
+++ pkg/advisor/main.go
@@ -52,7 +52,7 @@ func Run(o *Options) error {
 				return err
 			}
 
-			replicasets, err := client.AppsV1().ReplicaSets(namespace).List(ctx, metav1.ListOptions{
+			replicasets, err := client.AppsV1().ReplicaSets(deployment.Namespace).List(ctx, metav1.ListOptions{
 				LabelSelector: selector.String(),
 			})
 			if err != nil {
@@ -69,7 +69,7 @@ func Run(o *Options) error {
 				return err
 			}
 
-			pods, err := client.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{
+			pods, err := client.CoreV1().Pods(deployment.Namespace).List(ctx, metav1.ListOptions{
 				LabelSelector: selector.String(),
 			})
 			if err != nil {
@@ -124,14 +124,156 @@ func Run(o *Options) error {
 
 			cpuSave := float64(0.00)
 			memSave := float64(0.00)
-			data, cpuSave, memSave = o.analyzeDeployment(data, namespace, deployment, final)
+			data, cpuSave, memSave = o.analyzeDeployment(data, deployment, final)
+			totalCPUSave += cpuSave
+			totalMemSave += memSave
+		}
+
+		statefulSets, err := client.AppsV1().StatefulSets(namespace).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			return err
+		}
+
+		for _, statefulSet := range statefulSets.Items {
+			selector, err := metav1.LabelSelectorAsSelector(statefulSet.Spec.Selector)
+			if err != nil {
+				return err
+			}
+
+			pods, err := client.CoreV1().Pods(statefulSet.Namespace).List(ctx, metav1.ListOptions{
+				LabelSelector: selector.String(),
+			})
+			if err != nil {
+				return err
+			}
+
+			totalLimitCPU := make(map[string][]float64)
+			totalLimitMem := make(map[string][]float64)
+			totalRequestCPU := make(map[string][]float64)
+			totalRequestMem := make(map[string][]float64)
+
+			for _, pod := range pods.Items {
+				output, err := o.queryPrometheusForPod(ctx, promClient, pod)
+				if err != nil {
+					return err
+				}
+				for k, v := range output.RequestCPU {
+					totalRequestCPU[k] = append(totalRequestCPU[k], v)
+				}
+				for k, v := range output.RequestMem {
+					totalRequestMem[k] = append(totalRequestMem[k], v)
+				}
+				for k, v := range output.LimitCPU {
+					totalLimitCPU[k] = append(totalLimitCPU[k], v)
+				}
+				for k, v := range output.LimitMem {
+					totalLimitMem[k] = append(totalLimitMem[k], v)
+				}
+			}
+			final := prometheusMetrics{
+				LimitCPU:   make(map[string]float64),
+				LimitMem:   make(map[string]float64),
+				RequestCPU: make(map[string]float64),
+				RequestMem: make(map[string]float64),
+			}
+			for k, v := range totalRequestCPU {
+				scale := 10
+				value := float64Average(v)
+				final.RequestCPU[k] = math.Ceil(value*float64(scale)) / float64(scale)
+			}
+			for k, v := range totalRequestMem {
+				final.RequestMem[k] = math.Ceil(float64Average(v)/100) * 100
+			}
+			for k, v := range totalLimitCPU {
+				scale := 10
+				value := float64Average(v)
+				final.LimitCPU[k] = math.Ceil(value*float64(scale)) / float64(scale)
+			}
+			for k, v := range totalLimitMem {
+				final.LimitMem[k] = math.Ceil(float64Average(v)/100) * 100
+			}
+
+			cpuSave := float64(0.00)
+			memSave := float64(0.00)
+			data, cpuSave, memSave = o.analyzeStatefulset(data, statefulSet, final)
+			totalCPUSave += cpuSave
+			totalMemSave += memSave
+		}
+
+		daemonSets, err := client.AppsV1().DaemonSets(namespace).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			return err
+		}
+
+		for _, daemonSets := range daemonSets.Items {
+			selector, err := metav1.LabelSelectorAsSelector(daemonSets.Spec.Selector)
+			if err != nil {
+				return err
+			}
+
+			pods, err := client.CoreV1().Pods(daemonSets.Namespace).List(ctx, metav1.ListOptions{
+				LabelSelector: selector.String(),
+			})
+			if err != nil {
+				return err
+			}
+
+			totalLimitCPU := make(map[string][]float64)
+			totalLimitMem := make(map[string][]float64)
+			totalRequestCPU := make(map[string][]float64)
+			totalRequestMem := make(map[string][]float64)
+
+			for _, pod := range pods.Items {
+				output, err := o.queryPrometheusForPod(ctx, promClient, pod)
+				if err != nil {
+					return err
+				}
+				for k, v := range output.RequestCPU {
+					totalRequestCPU[k] = append(totalRequestCPU[k], v)
+				}
+				for k, v := range output.RequestMem {
+					totalRequestMem[k] = append(totalRequestMem[k], v)
+				}
+				for k, v := range output.LimitCPU {
+					totalLimitCPU[k] = append(totalLimitCPU[k], v)
+				}
+				for k, v := range output.LimitMem {
+					totalLimitMem[k] = append(totalLimitMem[k], v)
+				}
+			}
+			final := prometheusMetrics{
+				LimitCPU:   make(map[string]float64),
+				LimitMem:   make(map[string]float64),
+				RequestCPU: make(map[string]float64),
+				RequestMem: make(map[string]float64),
+			}
+			for k, v := range totalRequestCPU {
+				scale := 10
+				value := float64Average(v)
+				final.RequestCPU[k] = math.Ceil(value*float64(scale)) / float64(scale)
+			}
+			for k, v := range totalRequestMem {
+				final.RequestMem[k] = math.Ceil(float64Average(v)/100) * 100
+			}
+			for k, v := range totalLimitCPU {
+				scale := 10
+				value := float64Average(v)
+				final.LimitCPU[k] = math.Ceil(value*float64(scale)) / float64(scale)
+			}
+			for k, v := range totalLimitMem {
+				final.LimitMem[k] = math.Ceil(float64Average(v)/100) * 100
+			}
+
+			cpuSave := float64(0.00)
+			memSave := float64(0.00)
+			data, cpuSave, memSave = o.analyzeDaemonSet(data, daemonSets, final)
 			totalCPUSave += cpuSave
 			totalMemSave += memSave
 		}
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Namespace", "Deployment", "Container", "Request CPU (spec)", "Request MEM (spec)", "Limit CPU (spec)", "Limit MEM (spec)"})
+	table.SetHeader([]string{"Namespace", "Resource", "Container", "Request CPU (spec)", "Request MEM (spec)", "Limit CPU (spec)", "Limit MEM (spec)"})
 	for _, v := range data {
 		table.Append(v)
 	}
@@ -168,10 +310,68 @@ func currentValue(resources v1.ResourceRequirements, method string, resource v1.
 			return val.AsApproximateFloat64() - curSaving, val.String()
 		}
 	}
-	return -1*curSaving, "<nil>"
+	return -1 * curSaving, "<nil>"
 }
 
-func (o *Options) analyzeDeployment(data [][]string, namespace string, deployment appsv1.Deployment, finalMetrics prometheusMetrics) ([][]string, float64, float64) {
+func (o *Options) analyzeDaemonSet(data [][]string, daemonset appsv1.DaemonSet, finalMetrics prometheusMetrics) ([][]string, float64, float64) {
+	totalCPUSavings := float64(0.00)
+	totalMemSavings := float64(0.00)
+	for _, container := range daemonset.Spec.Template.Spec.Containers {
+		reqCpu := int(finalMetrics.RequestCPU[container.Name] * 1000)
+		reqMem := int(finalMetrics.RequestMem[container.Name])
+		limCpu := int(finalMetrics.LimitCPU[container.Name] * 1000)
+		limMem := int(finalMetrics.LimitMem[container.Name])
+
+		reqCpuSave, strReqCPU := currentValue(container.Resources, "request", v1.ResourceCPU, reqCpu, apresource.DecimalSI)
+		reqMemSave, strReqMem := currentValue(container.Resources, "request", v1.ResourceMemory, reqMem, apresource.BinarySI)
+		_, strLimCPU := currentValue(container.Resources, "limit", v1.ResourceCPU, limCpu, apresource.DecimalSI)
+		_, strLimMem := currentValue(container.Resources, "limit", v1.ResourceMemory, limMem, apresource.BinarySI)
+
+		totalCPUSavings += reqCpuSave * float64(daemonset.Status.DesiredNumberScheduled)
+		totalMemSavings += reqMemSave * float64(daemonset.Status.DesiredNumberScheduled)
+		data = append(data, []string{
+			daemonset.Namespace,
+			fmt.Sprintf("daemonset/%s", daemonset.Name),
+			container.Name,
+			fmt.Sprintf("%dm (%s)", reqCpu, strReqCPU),
+			fmt.Sprintf("%dMi (%s)", reqMem, strReqMem),
+			fmt.Sprintf("%dm (%s)", limCpu, strLimCPU),
+			fmt.Sprintf("%dMi (%s)", limMem, strLimMem),
+		})
+	}
+	return data, totalCPUSavings, totalMemSavings
+}
+
+func (o *Options) analyzeStatefulset(data [][]string, statefulset appsv1.StatefulSet, finalMetrics prometheusMetrics) ([][]string, float64, float64) {
+	totalCPUSavings := float64(0.00)
+	totalMemSavings := float64(0.00)
+	for _, container := range statefulset.Spec.Template.Spec.Containers {
+		reqCpu := int(finalMetrics.RequestCPU[container.Name] * 1000)
+		reqMem := int(finalMetrics.RequestMem[container.Name])
+		limCpu := int(finalMetrics.LimitCPU[container.Name] * 1000)
+		limMem := int(finalMetrics.LimitMem[container.Name])
+
+		reqCpuSave, strReqCPU := currentValue(container.Resources, "request", v1.ResourceCPU, reqCpu, apresource.DecimalSI)
+		reqMemSave, strReqMem := currentValue(container.Resources, "request", v1.ResourceMemory, reqMem, apresource.BinarySI)
+		_, strLimCPU := currentValue(container.Resources, "limit", v1.ResourceCPU, limCpu, apresource.DecimalSI)
+		_, strLimMem := currentValue(container.Resources, "limit", v1.ResourceMemory, limMem, apresource.BinarySI)
+
+		totalCPUSavings += reqCpuSave * float64(*statefulset.Spec.Replicas)
+		totalMemSavings += reqMemSave * float64(*statefulset.Spec.Replicas)
+		data = append(data, []string{
+			statefulset.Namespace,
+			fmt.Sprintf("statefulset/%s", statefulset.Name),
+			container.Name,
+			fmt.Sprintf("%dm (%s)", reqCpu, strReqCPU),
+			fmt.Sprintf("%dMi (%s)", reqMem, strReqMem),
+			fmt.Sprintf("%dm (%s)", limCpu, strLimCPU),
+			fmt.Sprintf("%dMi (%s)", limMem, strLimMem),
+		})
+	}
+	return data, totalCPUSavings, totalMemSavings
+}
+
+func (o *Options) analyzeDeployment(data [][]string, deployment appsv1.Deployment, finalMetrics prometheusMetrics) ([][]string, float64, float64) {
 	totalCPUSavings := float64(0.00)
 	totalMemSavings := float64(0.00)
 	for _, container := range deployment.Spec.Template.Spec.Containers {
@@ -188,8 +388,8 @@ func (o *Options) analyzeDeployment(data [][]string, namespace string, deploymen
 		totalCPUSavings += reqCpuSave * float64(*deployment.Spec.Replicas)
 		totalMemSavings += reqMemSave * float64(*deployment.Spec.Replicas)
 		data = append(data, []string{
-			namespace,
-			deployment.Name,
+			deployment.Namespace,
+			fmt.Sprintf("deployment/%s", deployment.Name),
 			container.Name,
 			fmt.Sprintf("%dm (%s)", reqCpu, strReqCPU),
 			fmt.Sprintf("%dMi (%s)", reqMem, strReqMem),
